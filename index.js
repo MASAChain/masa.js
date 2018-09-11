6377bab6d3e79b965c4f6fe9706bf39f1b0b6bee
Yukishige Nakajo <nakajo@haw.co.jp>
2018-09-11 10:35:55 +0900

    tweak: Get to run `npm test` env: introduce lint
    Preparing dev env now.

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-import masa from './lib/masa';
-export default masa;
+import masa from './lib/masa'
+export default masa
 
 // export const hoge = masa;
 
 // module.exports = masa;
 // console.log(module.exports);
 // module.exports.hoge = masa;
-
--- test/masa.spec.js
+++ test/masa.spec.js
@@ -1,9 +1,9 @@
-import masa from '../lib/masa';
-var assert = require('assert');
+import masa from '../lib/masa'
+var assert = require('assert')
 
 describe('masa.js', () => {
   it('should load', () => {
-    console.log(masa);
-    assert(masa);
-  });
-});
+    console.log(masa)
+    assert(masa)
+  })
+})
