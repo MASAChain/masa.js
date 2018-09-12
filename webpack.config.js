df782f4a7f99c3f0c0a6f4b03307b96c0eb18890
Yukishige Nakajo <nakajo@haw.co.jp>
2018-09-12 17:17:51 +0900

    impl: Implement Wallet. tweak webpack config and eslint config for test.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
   entry: ['./index.js'],
   mode: 'development',
   output: {
