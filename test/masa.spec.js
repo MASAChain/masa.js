d0f08bfd60b36323ad48b6692d0c56b6dd0a1333
Yukishige Nakajo <nakajo@haw.co.jp>
2018-09-18 15:13:06 +0900

    change: Change as per review from @k_taniguchi san. As first step, I change Masa class to have only functions of explorer.

--- test/masa.spec.js
+++ test/masa.spec.js
@@ -1,44 +1,19 @@
-import Masa from '../lib/masa'
+import masa from '../lib/masa'
 import { expect } from 'chai'
-import { GANACHE_MNEMONIC } from './util'
 
 describe('masa.js', () => {
-  it('should load', () => {
-    const masa = new Masa()
-    expect(masa).to.be.ok
+  describe('init', () => {
+    it('should load', () => {
+      expect(masa).to.be.ok
+    })
   })
   describe('masa point', () => {
-    let masaPoint
-    beforeEach(() => {
-      const masa = new Masa()
-      masaPoint = masa.point('0x1234567890abcdef1234')
-    })
     it('get masa point amount', () => {
-      expect(masaPoint.amountOf()).to.be.equal(500)
+      expect(masa.point.amountOf('0xaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbb')).to.be.equal(500)
+      expect(masa.point.amountOf('0x6861766520636f696e20697300000000000000b8')).to.be.equal(200)
     })
     it('get community masa point amount', () => {
-      expect(masaPoint.communityAmountOf(5)).to.be.equal(200)
-    })
-    it('get feedback transaction', () => {
-      const transaction = masaPoint.feedback('0x1111111111aaaaaaaaaa', 5, 1, 'nice guy')
-      expect(transaction).to.be.a('transaction')
-    })
-  })
-  describe('wallet functions', function() {
-    this.timeout(10000)
-    let masa
-    beforeEach(() => {
-      masa = new Masa()
-    })
-    it('generate new wallet', () => {
-      const wallet = masa.generateNewWallet()
-      expect(wallet).to.be.ok
-      expect(wallet).to.be.a('wallet')
-    })
-    it('restore wallet', () => {
-      const wallet = masa.restoreWallet(GANACHE_MNEMONIC)
-      expect(wallet).to.be.ok
-      expect(wallet).to.be.a('wallet')
+      expect(masa.point.communityAmountOf('0x6861766520636f696e20697300000000000000b8', 5)).to.be.equal(200)
     })
   })
 })
