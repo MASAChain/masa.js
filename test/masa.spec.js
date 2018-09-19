62dd44f8fd07fd536f41126ea7f3d73b09888a06
Yukishige Nakajo <nakajo@haw.co.jp>
2018-09-19 14:53:24 +0900

    impl: Implement masa.coin functions. integrate MasaCoin to Masa.

--- test/masa.spec.js
+++ test/masa.spec.js
@@ -1,5 +1,5 @@
 import masa from '../lib/masa'
-import { expect } from 'chai'
+import {expect} from 'chai'
 
 describe('masa.js', () => {
   describe('init', () => {
@@ -16,8 +16,19 @@ describe('masa.js', () => {
       expect(masa.point.communityAmountOf('0x6861766520636f696e20697300000000000000b8', 5)).to.be.equal(200)
     })
     it('masa.point.feedback is not support.', () => {
-      expect(function() {
+      expect(function () {
         masa.point.feedback('0x6861766520636f696e20697300000000000000b8', 'Challenge Caravan', 1, '')
+      }).to.throw('Not support function. Please use MasaTx.')
+    })
+  })
+  describe('masa coin', () => {
+    it('balanceOf', async() => {
+      const balance = await masa.coin.balanceOf('0xdaF8F49A3671082ED2ab0b67ecB7f0182fAEAb6C')
+      return expect(balance).to.be.equal(100)
+    })
+    it('unsupport transfer.', () => {
+      expect(function() {
+        masa.coin.transfer('0xdaF8F49A3671082ED2ab0b67ecB7f0182fAEAb6C', 20)
       }).to.throw('Not support function. Please use MasaTx.')
     })
   })
